076667add70bf6f0f333ebb5f36b4e0f63ae81a7
Amin <amin.bamavadat@gmail.com>
2021-01-19 16:45:09 -0500

    ADD: Add post functionaluty to the APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test APIView"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            "uses HTTP methods as function (get, post, pathc, put, delete)",
+            "is similar to a traditional Django View",
+            "gives you the most control over yor application logic",
+            "is mapped manually to urls"
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request, format=None):
+        """Creates A Hello message with our name"""
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_bad_request,
+            )
+
+
+
